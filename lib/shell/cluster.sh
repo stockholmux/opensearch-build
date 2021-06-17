a64694a53c82fbb2d90e51d032c71db759d25a98
Peter Zhu <zhujiaxi@amazon.com>
2021-06-17 14:51:52 -0400

    Add license headers to the scripts

--- lib/shell/cluster.sh
+++ lib/shell/cluster.sh
@@ -23,14 +23,9 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-# This is necessary for servers that try to run OpenSearch with docker
-# The hosts have to have these settings ready
-# Or the OpenSearch Process will not run
-sudo sysctl -w vm.max_map_count=262144
-ulimit -n 65535
-# Required for PA and similar processes that require shared memory to work
-sudo chmod -R 777 /dev/shm
+# This is a library of all OpenSearch/Dashboards cluster related functions
+# Source this file in your scripts
 
-# As for docker the host must have these as well in /etc/sysconfig/docker and restart service
-# OPTIONS="--default-ulimit nofile=65535:65535"
-# to replace the default limits of 1024:4096
+
+
+
