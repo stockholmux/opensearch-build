62275dceebb32e39a91c535a44b6d84ee504d7c9
Peter Zhu <zhujiaxi@amazon.com>
2021-06-18 17:43:07 -0400

    test

--- standard-test/deploy_single_tar_cluster.sh
+++ standard-test/deploy_single_tar_cluster.sh
@@ -94,13 +94,13 @@ while getopts ":hct:v:s:" arg; do
             exit 1
             ;;
         :)
-            echo -e "\nERROR: \"-${OPTARG}\" requires an argument"
-            echo "$0 -h for usage details of this script"
+            echo -e "\nERROR: '-${OPTARG}' requires an argument"
+            echo "'$0 -h' for usage details of this script"
             exit 1
             ;;
         ?)
-            echo -e "\nError: Invalid option \"-${OPTARG}\""
-            echo "$0 -h for usage details of this script"
+            echo -e "\nError: Invalid option '-${OPTARG}'"
+            echo "'$0 -h' for usage details of this script"
             exit 1
             ;;
     esac
@@ -109,7 +109,7 @@ done
 # Validate the required parameters to present
 if [ -z "$VERSION" ] || [ -z "$TYPE" ] || [ -z "$ENABLE_SECURITY" ]; then
     echo -e "\nERROR: You must specify '-v VERSION', '-t TYPE', '-s ENABLE_SECURITY'"
-    echo "$0 -h for usage details of this script"
+    echo "'$0 -h' for usage details of this script"
     exit 1
 else
     echo VERSION:$VERSION TYPE:$TYPE ENABLE_SECURITY:$ENABLE_SECURITY
